0679e55e832afeeb91718bcdbc4f76a4c71fd4ae
Mario Morales <mmario_93@hotmail.es>
2020-12-15 20:53:53 -0600

    V2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Practica 1 SA
+          Practica 1 SA - V2
         </p>
         <p>
           Mario Morales <strong>201213283</strong>
